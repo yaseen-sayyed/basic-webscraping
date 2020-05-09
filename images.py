86c3b4310f00c5859ecae8ad0c7841db5145572c
yaseen-sayyed <sayyed.yaseen97@gmail.com>
2020-05-09 16:13:10 +0530

    Second Commit

--- images.py
+++ images.py
@@ -6,20 +6,34 @@ from PIL import Image
 import os
 
 
-search = input("Search for: ")
-params = {"q": search}
+def start_search():
+    search = input("Search for: ")
+    params = {"q": search}
 
-r = requests.get("http://www.bing.com/images/search", params=params)
-soup = BeautifulSoup(r.text, "html.parser")
+    dir_name = search.replace(" ", "_").lower()
 
-links = soup.find_all("a", {"class": "thumb"})
+    if not os.path.isdir(dir_name):
+        os.makedirs("./scraped_images/" + dir_name)
 
-for item in links:
-    img_object = requests.get(item.attrs["href"])
-    title = item.attrs["href"].split("/")[-1].partition("?")[0]
-    try:
-        img = Image.open(BytesIO(img_object.content))
-    except PIL.UnidentifiedImageError:
-        print("Error in image")
-        continue
-    img.save("./scraped_images/" + title)
+    r = requests.get("http://www.bing.com/images/search", params=params)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    links = soup.find_all("a", {"class": "thumb"})
+
+    for item in links:
+        try:
+            img_object = requests.get(item.attrs["href"])
+            print("Getting", item.attrs["href"])
+            title = item.attrs["href"].split("/")[-1].partition("?")[0]
+            try:
+                img = Image.open(BytesIO(img_object.content))
+                img.save("./" + dir_name + "/" + title)
+            except (PIL.UnidentifiedImageError, OSError):
+                print("Erroneous image or couldn't save image.")
+        except:
+            print("Could not get image.")
+
+    start_search()
+
+
+start_search()
